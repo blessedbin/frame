ed2748dd6de88ddadb60aca012b0fc81ede9e5e3
xubin <370756465@qq.com>
2018-09-26 17:13:59 +0800

    拆分鉴权服务，单独独立成一个微服务

---
+++ frame-common/src/main/java/com/blessedbin/frame/common/Main.java
@@ -0,0 +1,14 @@
+package com.blessedbin.frame.common;
+
+/**
+ * Created by xubin on 2018/9/26.
+ *
+ * @author 37075
+ * @date 2018/9/26
+ * @time 15:16
+ * @tool intellij idea
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
