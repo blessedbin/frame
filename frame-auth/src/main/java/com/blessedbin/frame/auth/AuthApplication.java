295d0a2b27dc887bcfa73a239dd4003f37cae472
xubin <370756465@qq.com>
2018-09-26 15:08:28 +0800

    拆分鉴权服务，单独独立成一个微服务

---
+++ frame-auth/src/main/java/com/blessedbin/frame/auth/AuthApplication.java
@@ -0,0 +1,25 @@
+package com.blessedbin.frame.auth;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 鉴权服务
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTransactionManagement
+@Log4j2
+@EnableAspectJAutoProxy
+@EnableFeignClients
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
