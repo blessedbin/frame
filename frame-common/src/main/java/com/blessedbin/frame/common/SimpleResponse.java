7f36065e2f596724b94ea6e7b9a2f5de759e7514
blessedbin <xubin740723@gmail.com>
2018-10-12 17:14:03 +0800

    新增管理员重置用户密码操作

--- frame-common/src/main/java/com/blessedbin/frame/common/SimpleResponse.java
+++ frame-common/src/main/java/com/blessedbin/frame/common/SimpleResponse.java
@@ -4,10 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
-import org.springframework.util.MultiValueMap;
 
 import java.util.List;
 
