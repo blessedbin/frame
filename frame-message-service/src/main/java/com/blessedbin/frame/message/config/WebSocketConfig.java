311fa7fcdc5bd96982fa44f7286c226b3ebc913d
blessedbin <xubin740723@gmail.com>
2018-11-15 17:33:29 +0800

    完善

---
+++ frame-message-service/src/main/java/com/blessedbin/frame/message/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.blessedbin.frame.message.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * Created by xubin on 2018/11/14.
+ *
+ * @author 37075
+ * @date 2018/11/14
+ * @time 22:46
+ * @tool intellij idea
+ */
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+
+}
